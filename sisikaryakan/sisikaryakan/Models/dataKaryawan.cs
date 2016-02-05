ec00b2dfa4e853cf56695c25ee51dbfb60c15a8a
sofifitriyah <sofi.fitriyah@basajans.com>
2016-02-05 15:57:27 +0700

    Create Employee.cs

---
+++ sisikaryakan/sisikaryakan/Models/dataKaryawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sisikaryakan.Models
+{
+    public class dataKaryawan
+    {
+        public int employeeId { get; set; }
+       
+        public string employeeName { get; set; }
+
+        public string gender { get; set; }
+
+        public DateTime birthDate { get; set; }
+
+        public string adress { get; set; }
+
+        public string religion { get; set; }
+
+        public DateTime entryDate { get; set; }
+
+        public string status { get; set; }
+
+        public int basicSalary { get; set; }
+
+        public string phoneNumber { get; set; }
+    }
+}
