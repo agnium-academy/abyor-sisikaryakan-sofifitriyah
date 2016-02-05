41ea62ab4a9f812c7fccd41628e91525baaa3465
sofifitriyah <sofi.fitriyah@basajans.com>
2016-02-05 16:07:37 +0700

    Update Employee.cs with required data

--- sisikaryakan/sisikaryakan/Models/Employee.cs
+++ sisikaryakan/sisikaryakan/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,24 +8,35 @@ namespace sisikaryakan.Models
 {
     public class Employee
     {
+        [Required]
         public int employeeId { get; set; }
-       
+
+        [Required]
         public string employeeName { get; set; }
 
+        [Required]
         public string gender { get; set; }
 
+        [Required]
         public DateTime birthDate { get; set; }
 
+        [Required]
         public string adress { get; set; }
 
+
+        [Required]
         public string religion { get; set; }
 
+        [Required]
         public DateTime entryDate { get; set; }
 
+        [Required]
         public string status { get; set; }
 
+        [Required]
         public int basicSalary { get; set; }
 
+        [Required]
         public string phoneNumber { get; set; }
     }
 }
